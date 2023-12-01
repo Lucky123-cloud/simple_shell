a81184f2a48a9bad6ce04a23bbd3f66b9495e35a
Lucky <luckybaraka21@gmail.com>
2023-12-01 01:12:51 -0800

    Lucky added AUTHORS file

---
+++ shell.h
@@ -0,0 +1,18 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 256
+#define ENV_SEPARATOR "="
+#define ESCAPE_SEPARATOR "#"
+#define PATH_SEPARATOR ":"
+#define COMMAND_SEPARATOR ";\n"
+#define SEPARATORS "\n"
+#define PROMPT "$"
