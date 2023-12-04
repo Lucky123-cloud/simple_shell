994e98735a2415804b2e173218182142afe908cb
thattechgirlie <wanji.njoro1@gmail.com>
2023-12-04 04:58:41 -0800

    digits

---
+++ isdigit.c
@@ -0,0 +1,10 @@
+#include "shell.h"
+/**
+ * _isdigit - checks for digits
+ * @prnch: prints character
+ * Return: returns value
+ */
+int _isdigit(char prnch)
+{
+	return (prnch >= '0' && prnch <= '9');
+}
