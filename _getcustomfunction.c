5ce12734ffd276f3a4fc62a0b4278ee1d72beef8
Lucky <luckybaraka21@gmail.com>
2023-12-05 03:53:27 -0800

    I love my work

---
+++ _getcustomfunction.c
@@ -0,0 +1,31 @@
+#include "shell.h"
+
+/**
+ * _getcustomfunc - check custom command
+ *
+ * @prnCommand: command
+ *
+ * Return: pointer function
+ */
+void (*_getcustomfunc(char *prnCommand))(appData_t *)
+{
+	int i = 0;
+
+	customCommand_t fp[] = {
+		{"cd", _cd},
+		{"env", _env},
+		{"setenv", _setEnvironment},
+		{"unsetenv", _unsetEnvironment},
+		{"exit", _exits},
+		{"help", _help}
+	};
+
+	while (i < 6)
+	{
+		if (_strcmp(prnCommand, (fp + i)->commandName) == 0)
+			return ((fp + i)->func);
+		i++;
+	}
+
+	return (NULL);
+}
