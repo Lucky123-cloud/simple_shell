dfdd0c898ecb3826b3a90d880c8c575673659a6f
thattechgirlie <wanji.njoro1@gmail.com>
2023-12-15 03:21:12 -0800

    thattechgirlie

--- environ.c
+++ environ.c
@@ -1,92 +1,86 @@
 #include "shell.h"
 
 /**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
+ * _myenv - checks the environment
+ * @adr: gives us address parameter
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int _myenv(info_t *adr)
 {
-	print_list_str(info->env);
+	print_list_str(adr->env);
 	return (0);
 }
 
 /**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
+ * _getenv - environment variable
+ * @adr: checks the parameter structure
+ * @b: parameter character
+ * Return: returns null
  */
-char *_getenv(info_t *info, const char *name)
+char *_getenv(info_t *adr, const char *b)
 {
-	list_t *node = info->env;
-	char *p;
+	list_t *node = adr->env;
+	char *k;
 
 	while (node)
 	{
-		p = starts_with(node->str, name);
-		if (p && *p)
-			return (p);
+		k = starts_with(node->str, b);
+		if (k && *k)
+			return (k);
 		node = node->next;
 	}
 	return (NULL);
 }
 
 /**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _mysetenv - sets environment
+ * @adr: checks the parameter 1
+ *  Return: returns 0
  */
-int _mysetenv(info_t *info)
+int _mysetenv(info_t *adr)
 {
-	if (info->argc != 3)
+	if (adr->argc != 3)
 	{
 		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (_setenv(adr, adr->argv[1], adr->argv[2]))
 		return (0);
 	return (1);
 }
 
 /**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
+ * _myunsetenv - unset environment variable
+ * @adr: address of the parameter
+ * Return: returns 0
  */
-int _myunsetenv(info_t *info)
+int _myunsetenv(info_t *adr)
 {
-	int i;
+	int a;
 
-	if (info->argc == 1)
+	if (adr->argc == 1)
 	{
 		_eputs("Too few arguements.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+	for (a = 1; a <= adr->argc; a++)
+		_unsetenv(adr, adr->argv[i]);
 
 	return (0);
 }
 
 /**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
+ * populate_env_list - gives us linked list
+ * @adr: structure parsmeter
+ * Return: returns 0
  */
-int populate_env_list(info_t *info)
+int populate_env_list(info_t *adr)
 {
 	list_t *node = NULL;
-	size_t i;
+	size_t l;
 
-	for (i = 0; environ[i]; i++)
-		add_node_end(&node, environ[i], 0);
-	info->env = node;
+	for (l = 0; environ[l]; l++)
+		add_node_end(&node, environ[l], 0);
+	adr->env = node;
 	return (0);
 }
